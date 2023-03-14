558475a867ab82daa88396528414842ec43c94fa
Alex B Carr <mralex.carr@yahoo.com>
2023-03-13 18:55:08 -0700

    Added initial react app

---
+++ src/HornedBeast.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+class HornedBeast  extends React.Component {
+  render(){
+    return (
+      <article>
+
+        <h2>{this.props.title}</h2>
+        <img src="{this.props.image_url}" alt=""></img>
+        <p>{this.props.description}</p>
+
+      </article>
+    )
+  }
+} 
+
+
+
+export default HornedBeast; 
