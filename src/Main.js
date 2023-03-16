c023e853bb99dd7545e2486eb390dca99a9c793d
Alex B Carr <mralex.carr@yahoo.com>
2023-03-15 20:07:16 -0700

    Lab 3

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-import data from './data/data.json';
+import SelectedBeast from './SelectedBeast';
 import './Main.css'
 
 class Main extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    
+    }
+  }
   render(){
     return (
       <main>
       
-        {data.map((hornedBeast,id) => {
-          return <HornedBeast title={hornedBeast.title} image_url={hornedBeast.image_url} description={hornedBeast.description} key={id} />
+        {this.props.data.map((hornedBeast,id) => {
+          return <HornedBeast title={hornedBeast.title} image_url={hornedBeast.image_url} description={hornedBeast.description} key={id} handleOpenModal={this.props.handleOpenModal}/>
         })}
       </main>
     )
